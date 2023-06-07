6ba85c2c83201442af2cb46f0a6e57e5158b90ee
Akash Lakhera <akashlakhera05@gmail.com>
2023-06-07 16:20:09 +0530

    remove unused import

--- src/component/flights/ConfirmBooking.js
+++ src/component/flights/ConfirmBooking.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import PaymentForm from "../payment/PaymentForm";
 import { loadScript } from "../../config/Utils";
 
 const ConfirmBooking = () => {
